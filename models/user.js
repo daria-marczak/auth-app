7a6afd49f42ab25d1f0f8dd527c4b02e6acd7f79
daria marczak <marczak.ria@gmail.com>
2018-09-30 16:31:55 +0200

    Created user schema

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Schema is what we use when we tell mongoose about the fields that a particular model is going to have
+
+// Define the modal
+const userSchema = new Schema({
+  email: { type: String, unique: true, lowecase: true },
+  password: String
+});
+
+// Mongoose before saving the record to the database will first verify if the email is unique also taking into account lowercase, as without this declaration it would allow capitalcase to be different from lowercase
+
+// Create the model class
+const ModelClass = mongoose.model("user", userSchema);
+// This loads the schema to mongoose and the schema corresponds to the "user" entry in DB.
+
+// Export the model
+modules.exports = ModelClass;
