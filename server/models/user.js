5cd649d35e9c3dac20dad8dc70ce65bfc4919bce
daria marczak <marczak.ria@gmail.com>
2018-10-01 19:49:11 +0200

    Added notes on bcrypt

--- server/models/user.js
+++ server/models/user.js
@@ -13,20 +13,31 @@ const userSchema = new Schema({
 // Mongoose before saving the record to the database will first verify if the email is unique also taking into account lowercase, as without this declaration it would allow capitalcase to be different from lowercase
 
 // On Save hook, encrypt password
+// Before saving a model, run this function
 userSchema.pre("save", function(next) {
   const user = this;
+  // The context is the user model
 
+
+  // Generate a salt, what takes some amount of time, then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) {
       return next(err);
     }
 
+    // Encrypt the password using the salt, what also takes some amount of time
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) { return next(err) }
+
+      // Overwrite plain text password with encrypted password
       user.password = hash;
+
+      // This basically means: go ahead and save the model
       next();
     });
   });
+
+  // When saving a password, we generate a salt (a randomly generator string of characters). By combining it with the plain password, we get the encrypted password.
 });
 
 
