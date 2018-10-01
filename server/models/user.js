66a863d471e28ceaf2d9f48fb4bfb4a88148fbef
daria marczak <marczak.ria@gmail.com>
2018-10-01 19:42:50 +0200

    Encryptying password

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 // Schema is what we use when we tell mongoose about the fields that a particular model is going to have
 
@@ -10,6 +11,25 @@ const userSchema = new Schema({
 });
 
 // Mongoose before saving the record to the database will first verify if the email is unique also taking into account lowercase, as without this declaration it would allow capitalcase to be different from lowercase
+
+// On Save hook, encrypt password
+userSchema.pre("save", function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err) }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
 
 // Create the model class
 const ModelClass = mongoose.model("user", userSchema);
