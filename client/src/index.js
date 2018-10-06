7658622288e182025e2b66e80357b59ea99274a7
daria marczak <marczak.ria@gmail.com>
2018-10-06 16:17:59 +0200

    Wired up sign up component

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import App from "./components/App";
 import Welcome from "./components/Welcome";
+import SignUp from "./components/auth/SignUp";
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Route path="/" exact component={Welcome} />
+      <Route path="/signup" exact component={SignUp} />
     </App>
   </BrowserRouter>,
   document.querySelector("#root")
