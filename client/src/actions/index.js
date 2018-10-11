8855ef1e740692e7e0c5440a95f89559ba5f855a
daria marczak <marczak.ria@gmail.com>
2018-10-11 20:02:21 +0200

    Redirecting user on signup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { AUTH_USER, AUTH_ERROR } from "./types";
 
-export const signup = formProps => async dispatch => {
+export const signup = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post(
       "http://localhost:3090/signup",
@@ -10,6 +10,8 @@ export const signup = formProps => async dispatch => {
     );
 
     dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
+    // This is how we redirect the user
   } catch (e) {
     dispatch({ type: AUTH_ERROR, payload: "Email in use" });
   }
