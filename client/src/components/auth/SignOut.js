bad9d27e6b900b6d32f774f1ae072488284682f1
daria marczak <marczak.ria@gmail.com>
2018-10-11 20:24:58 +0200

    Sign out function is working

--- client/src/components/auth/SignOut.js
+++ client/src/components/auth/SignOut.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as actions from "../../actions";
+
 
 class SignOut extends Component {
+  componentDidMount() {
+    this.props.signout();
+  }
+
   render() {
     return <div>Sorry to see you go</div>;
   }
 }
 
-export default SignOut;
+export default connect(null, actions)(SignOut);
